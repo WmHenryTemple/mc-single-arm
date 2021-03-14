78fdd985e118cb6349653689c2c4094baa4bd4cc
wmhenrytemple <billyh@temple.edu>
2021-03-14 10:00:34 -0400

    more infiles and old hms recon

--- runscripts/simHms.sh
+++ runscripts/simHms.sh
@@ -2,42 +2,42 @@
 cd /u/group/shms/wmhenry/mc-single-arm
 pwd
 
-run_mc_single_arm hms_mc_h21deg3p3
-run_mc_single_arm hms_mc_h21deg4p0
-run_mc_single_arm hms_mc_h21deg4p5
-run_mc_single_arm hms_mc_h21deg5p1
-run_mc_single_arm hms_mc_h21deg5p7
+run_mc_single_arm hms_h21deg3p3
+run_mc_single_arm hms_h21deg4p0
+run_mc_single_arm hms_h21deg4p5
+run_mc_single_arm hms_h21deg5p1
+run_mc_single_arm hms_h21deg5p7
 
-run_mc_single_arm hms_mc_d21deg3p3
-run_mc_single_arm hms_mc_d21deg4p0
-run_mc_single_arm hms_mc_d21deg4p5
-run_mc_single_arm hms_mc_d21deg5p1
-run_mc_single_arm hms_mc_d21deg5p7
+run_mc_single_arm hms_d21deg3p3
+run_mc_single_arm hms_d21deg4p0
+run_mc_single_arm hms_d21deg4p5
+run_mc_single_arm hms_d21deg5p1
+run_mc_single_arm hms_d21deg5p7
 
-run_mc_single_arm hms_mc_c21deg3p3
-run_mc_single_arm hms_mc_c21deg4p0
-run_mc_single_arm hms_mc_c21deg4p5
-run_mc_single_arm hms_mc_c21deg5p1
-run_mc_single_arm hms_mc_c21deg5p7
+#run_mc_single_arm hms_c21deg3p3
+#run_mc_single_arm hms_c21deg4p0
+#run_mc_single_arm hms_c21deg4p5
+#run_mc_single_arm hms_c21deg5p1
+#run_mc_single_arm hms_c21deg5p7
 
 source /site/12gev_phys/softenv.csh 2.1
 
 cd worksim
 
-h2root hms_mc_h21deg3p3.rzdat
-h2root hms_mc_h21deg4p0.rzdat
-h2root hms_mc_h21deg4p5.rzdat
-h2root hms_mc_h21deg5p1.rzdat
-h2root hms_mc_h21deg5p7.rzdat
+h2root hms_h21deg3p3.rzdat
+h2root hms_h21deg4p0.rzdat
+h2root hms_h21deg4p5.rzdat
+h2root hms_h21deg5p1.rzdat
+h2root hms_h21deg5p7.rzdat
 
-h2root hms_mc_d21deg3p3.rzdat
-h2root hms_mc_d21deg4p0.rzdat
-h2root hms_mc_d21deg4p5.rzdat
-h2root hms_mc_d21deg5p1.rzdat
-h2root hms_mc_d21deg5p7.rzdat
+h2root hms_d21deg3p3.rzdat
+h2root hms_d21deg4p0.rzdat
+h2root hms_d21deg4p5.rzdat
+h2root hms_d21deg5p1.rzdat
+h2root hms_d21deg5p7.rzdat
 
-h2root hms_mc_c21deg3p3.rzdat
-h2root hms_mc_c21deg4p0.rzdat
-h2root hms_mc_c21deg4p5.rzdat
-h2root hms_mc_c21deg5p1.rzdat
-h2root hms_mc_c21deg5p7.rzdat
+#h2root hms_c21deg3p3.rzdat
+#h2root hms_c21deg4p0.rzdat
+#h2root hms_c21deg4p5.rzdat
+#h2root hms_c21deg5p1.rzdat
+#h2root hms_c21deg5p7.rzdat
